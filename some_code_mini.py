6e8f09672645b049dbde6c04ac8d8a0ff940e2d7
TomV <tom@viner.tv>
2015-03-05 20:59:14 +0000

    adding README and a file for testing

---
+++ some_code_mini.py
@@ -0,0 +1,19 @@
+
+import s
+import a
+import m
+s = []
+v = [1, 2]
+def a(x):
+    v = [3, 4]
+    v.append(x)
+    return v
+def a(x):
+    v = [3, 4]
+    x.extend(v)
+    return v
+print v
+print a(v)
+print v
+print a(v)
+print v
