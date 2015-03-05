587987eeffc15fedc3bbfd4f3e74b1a5ca038063
Paul Egan <paulegan@mail.com>
2015-03-05 20:28:15 +0000

    Added first step

--- minify.py
+++ minify.py
@@ -3,7 +3,16 @@ import ast
 import meta
 
 
-steps = [lambda x: x]
+def replace_variables(code):
+    for node in ast.walk(code):
+        if hasattr(node, 'id'):
+            node.id = node.id[0]
+        elif hasattr(node, 'name'):
+            node.name = node.name[0]
+    return code
+
+
+steps = [replace_variables]
 
 
 def minify(filename):
@@ -15,4 +24,8 @@ def minify(filename):
     return meta.dump_python_source(result)
 
 if __name__ == '__main__':
-    print minify(sys.argv[1])
+    filename = sys.argv[-1]
+    if sys.argv[1] == '-t':
+        exec(minify(filename))
+    else:
+        print minify(filename)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 this_is_my_long_var = 1
 this_is_my_long_var += 1
-def this_is_my_verbosely_named_function(with_a_very_long_named_arg):
+def xthis_is_my_verbosely_named_function(with_a_very_long_named_arg):
     return with_a_very_long_named_arg + 1
-print this_is_my_verbosely_named_function(this_is_my_long_var)
+print xthis_is_my_verbosely_named_function(this_is_my_long_var)
