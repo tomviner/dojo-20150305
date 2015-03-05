940c35665f462d7fae5f28e96b8830139410301e
TomV <tom@viner.tv>
2015-03-05 21:25:35 +0000

    better example code to minify

--- some_code.py
+++ some_code.py
@@ -5,17 +5,20 @@ import meta
 
 steps = []  # [replace_variables, remove_whitespace]
 
-var = [1, 2]
+var = [1, 1 + 2]
 
-def append_me(x):
+def append_me(args):
     var = [3, 4]
-    var.append(x)
+    var.append(args)
     return var
 
 def append_to_me(x):
     var = [3, 4]
     x.extend(var)
     return var
+
+def star(*args):
+    print args
 
 print var
 print append_me(var)
--- test_mini.py
+++
@@ -1,6 +0,0 @@
-
-b = 1
-b += 1
-def a(c):
-    return (c + 1)
-print a(b)
